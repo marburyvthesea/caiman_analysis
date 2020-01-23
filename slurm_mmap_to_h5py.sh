9ab1da0085c2dd35fafba86b0e2497fbc92fa6e6
John Marshall <jjm2128@gmail.com>
2020-01-23 16:50:20 -0600

    changes to find python envs in projects directory

--- slurm_mmap_to_h5py.sh
+++ slurm_mmap_to_h5py.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH -A p30771
 #SBATCH -p short
-#SBATCH -t 01:00:00
+#SBATCH -t 00:30:00
 #SBATCH -o ./logfiles/slurm.%x-%j.out # STDOUT
 #SBATCH --job-name="mmap_to_h5py"
 #SBATCH -N 1
-#SBATCH -n 5
+#SBATCH -n 2
 #SBATCH --mem=90G
 
 cd ~
@@ -18,14 +18,13 @@ module load python/anaconda3.6
 
 #need to cd to load conda environment
 
-cd pythonenvs
-cd CaImAn
+cd /projects/p30771/pythonenvs/CaImAn/
 source activate caiman
 
 #need to cd to module directory
 
-cd /home/jma819/caiman_data/demos/notebooks/caiman_analysis
+cd /home/jma819/caiman_quest/demos/notebooks/caiman_analysis
 
 #inputs are path to directory containing files, number or processors to use and length of batches to concactenate into one file
 
-python mmap_to_h5py.py /projects/p30771/miniscope/data/GRIN033/H13_M42_S33/mmap00_05/ 5 1 
+python mmap_to_h5py.py /projects/p30771/miniscope/data/GRIN032/H16_M49_S22/mmap/test/ 2 1 
